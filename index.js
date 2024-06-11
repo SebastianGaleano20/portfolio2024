cd3c509420478c4e7fe5a84fd3608b44fc88053c
Sebastian Galeano <sebascarp9@gmail.com>
2024-06-11 10:19:43 -0300

    error url add

--- index.js
+++ index.js
@@ -11,10 +11,13 @@ app.set('view engine', 'ejs');
 app.use(express.static('assets'));
 app.use(express.static('public'));
 
-app.get('/',(_request,response) => {
-    response.render('./index')
-})
 
+app.get('https://sebastian-portfolio.vercel.app/',(_request,response) => { 
+    response.render('./index');
+})
+app.get('*', (_request,response)=>{
+    response.send('404 NOT FOUND')
+})
 app.listen(PORT,() =>{
     console.log(`App listening on port ${PORT}`);
 })
